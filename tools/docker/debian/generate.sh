527b367ba2386aa496ed256269a26086d324fff4
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2023-09-14 22:56:43 -0700

    Refactor Dockerfile to use apt-get install for bioinformatics tools available from there

--- tools/docker/debian/generate.sh
+++ tools/docker/debian/generate.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+rm -rf ../../../images/
 ./build-container.sh && \
 	./build-state.js && \
 	zstd -f ../../../images/debian-state-base.bin && \
 	cp ../../../images/debian-state-base.bin.zst ../../../../sandbox.bio/static/v86/ && \
+	rm -rf ../../../../sandbox.bio/static/v86/debian-9p-rootfs-flat/ && \
 	cp -n -r ../../../images/debian-9p-rootfs-flat/ ../../../../sandbox.bio/static/v86/debian-9p-rootfs-flat/
